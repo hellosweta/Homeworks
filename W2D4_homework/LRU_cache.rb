eabe0a9fc4a1374bf2be941e4e4a9bdab1662460
App Academy Student <student@appacademy.io>
2017-02-02 21:15:53 -0800

    Implement LRU cache using array

---
+++ W2D4_homework/LRU_cache.rb
@@ -0,0 +1,59 @@
+class LRUCache
+   def initialize(size)
+     @size = size
+     @cache = []
+   end
+
+   def count
+     @cache.length
+     # returns number of elements currently in cache
+   end
+
+   def add(el)
+    if @cache.include?(el)
+      @cache.delete(el)
+      @cache << el
+    else
+      if @cache.length < @size
+        @cache << el
+      else
+        @cache.shift
+        @cache << el
+      end
+    end
+
+
+    el
+
+     # adds element to cache according to LRU principle
+   end
+
+   def show
+     # shows the items in the cache, with the LRU item first
+     p @cache
+     nil
+   end
+
+   private
+   # helper methods go here!
+
+ end
+
+  johnny_cache = LRUCache.new(4)
+
+   johnny_cache.add("I walk the line")
+   johnny_cache.add(5)
+
+   johnny_cache.count # => returns 2
+
+   johnny_cache.add([1,2,3])
+   johnny_cache.add(5)
+   johnny_cache.add(-5)
+   johnny_cache.add({a: 1, b: 2, c: 3})
+   johnny_cache.add([1,2,3,4])
+   johnny_cache.add("I walk the line")
+   johnny_cache.add(:ring_of_fire)
+   johnny_cache.add("I walk the line")
+   johnny_cache.add({a: 1, b: 2, c: 3})
+
+   johnny_cache.show
